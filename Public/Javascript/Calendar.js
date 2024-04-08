967376c30fa4002c00d03b702611580277f7dfbf
Vraiz <82005765+Vraiz@users.noreply.github.com>
2024-04-08 15:07:15 +0800

    Update Calendar.js

--- Public/Javascript/Calendar.js
+++ Public/Javascript/Calendar.js
@@ -1,115 +1,430 @@
-let nav = 0;
-let userID = localStorage.getItem('currentUserID'); // Change from objectId to userID
-const backDrop = document.getElementById('modalBackDrop');
-const calendar = document.getElementById('calendar');
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-function formatDateToISO(day, month, year) {
-    const formattedMonth = month < 10 ? '0' + month : month.toString();
-    const formattedDay = day < 10 ? '0' + day : day.toString();
-    return `${year}-${formattedMonth}-${formattedDay}`;
+function goToCalendar() {
+  window.location.href = '/pages/Calendar';
+}
+function goToCharacters() {
+  window.location.href = '/pages/Character';
+}
+function goToGacha() {
+  window.location.href = '/pages/gacha';
+}
+function goToStore() {
+  window.location.href = '/pages/Store';
 }
 
-let tasksByDate = {}; // Object to hold tasks organized by their due dates
-
-function fetchTasks() {
-    return fetch('/tasks/getUser/' + userID)
-        .then(response => response.json())
-        .then(tasks => {
-            tasksByDate = {}; // Reset tasksByDate
-            tasks.forEach(task => {
-                const dueDate = new Date(task.taskDateDue).toISOString().split('T')[0]; // Use ISO string to avoid locale issues
-                if (!tasksByDate[dueDate]) {
-                    tasksByDate[dueDate] = [];
-                }
-                tasksByDate[dueDate].push(task);
-            });
-        })
-        .catch(error => console.error('Error fetching tasks:', error));
+function logout() {
+  localStorage.removeItem('currentUserID');
+  window.location.href = '/pages/landingpage';
+}
+function closeProfileModal() {
+  document.getElementById('profileModal').style.display = 'none';
+}
+function hideModal() {
+  document.getElementById("form").style.display = "none";
 }
 
-function load() {
-    const dt = new Date();
+const readData = async () => {
+  let userID = localStorage.getItem("currentUserID")
+  let rollDataR = await fetch('/userdatas/' + userID)
+  let rollData = await rollDataR.json();
+  return rollData;
+}
 
-    if (nav !== 0) {
-        dt.setMonth(new Date().getMonth() + nav);
+let userID
+
+let fetchUserData = async () => {
+  try {
+    let userID = localStorage.getItem('currentUserID');
+    // Fetch user data from the database
+    const response = await fetch(`/userdatas/${userID}`); // Assuming you have an endpoint to retrieve user data
+    if (!response.ok) {
+      throw new Error('Failed to fetch user data');
     }
+    const userData = await response.json();
 
-    const year = dt.getFullYear();
-    const month = dt.getMonth();
-    const firstDayOfMonth = new Date(year, month, 1);
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    // Populate profile information in the modal
+    document.getElementById("profileName").innerText = userData.userName;
+    document.getElementById("profileEmail").innerText = userData.email;
+    document.getElementById("profileDateCreated").innerText = userData.dateCreated;
+    document.getElementById("profileTasksCompleted").innerText = userData.TotalTasksCompleted;
+    document.getElementById("coins").innerText = userData.credits;
 
-    const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-    });
-    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+    // Display the modal
+    document.getElementById("profileModal").style.display = "block";
+  } catch (err) {
+    console.error("Error fetching user data:", err);
+  }
+};
 
-    document.getElementById('monthDisplay').innerText = `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+function openProfileModal() {
+  // Display the modal
+  document.getElementById('profileModal').style.display = 'block';
+  fetchUserData();
+}
 
-    calendar.innerHTML = '';
+// Task form handling
+let form = document.getElementById("form");
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // Always prevent default form submission
+  let isFormValid = formValidation();
 
-    fetch('/tasks/getUser/' + userID)
-    .then(response => response.json())
-    .then(tasks => {
-        tasks = tasks.filter(task => !task.isTaskDeleted && task.taskStatus !== 'Complete');
-        for (let i = 1; i <= paddingDays + daysInMonth; i++) {
-            const daySquare = document.createElement('div');
-            daySquare.classList.add('day');
+  if (isFormValid) {
+    if (currentTaskID) {
+      updateTask(currentTaskID);
+    } else {
+      createNewTask();
+    }
+    // Assuming Bootstrap 5, hide the modal like this:
+    var modalInstance = bootstrap.Modal.getInstance(document.getElementById('form').closest('.modal'));
+    modalInstance.hide();
+  }
+  // If form is not valid, the modal will stay open for corrections
+});
 
-            if (i > paddingDays) {
-                const day = i - paddingDays;
-                const isoDayString = formatDateToISO(day, month + 1, year);
-                daySquare.innerText = day;
 
-                // Filter tasks for this date and append them with class 'event'
-                const tasksForDay = tasks.filter(task => new Date(task.taskDateDue).toISOString().split('T')[0] === isoDayString);
-                tasksForDay.forEach(task => {
-                    const taskElement = document.createElement('div');
-                    taskElement.classList.add('event'); // Apply 'event' class for styling
-                    taskElement.textContent = task.taskName; // Simplify for demonstration
+let formValidation = () => {
+  let isValid = true; // Assume form is valid
 
-                    const taskDueDate = new Date(task.taskDateDue);
-                    const currentDate = new Date();
-                    currentDate.setHours(0,0,0,0); // Reset hours for accurate comparison
+  let textInput = document.getElementById("textInput");
+  let dateInput = document.getElementById("dateInput");
+  let textArea = document.getElementById("textArea");
+  let msg = document.getElementById("msg");
+  let difficulty = document.getElementById("difficulty");
+  let statusInput = document.getElementById("statusInput");
 
-                    if (task.taskStatus === 'Started') {
-                        taskElement.classList.add('started'); // Add 'started' class for started tasks
-                    } else if (taskDueDate < currentDate) {
-                        taskElement.classList.add('overdue'); // Add 'overdue' class for overdue tasks
-                    }
+  let inputDate = new Date(dateInput.value);
+  let today = new Date();
+  today.setHours(0,0,0,0); // Reset the time part to ensure only the date is compared
 
-                    daySquare.appendChild(taskElement);
-                });
-                
-            } else {
-                daySquare.classList.add('padding');
-            }
+  if (textInput.value === "") {
+    msg.innerHTML = "Task cannot be blank!";
+    isValid = false; // Form is invalid
+  } else if (dateInput.value === "" || inputDate < today) {
+    msg.innerHTML = "Due Date cannot be blank or in the past!";
+    isValid = false; // Form is invalid
+  } else if (difficulty.value === "Choose a Difficulty") {
+    msg.innerHTML = "You must choose a difficulty!";
+    isValid = false; // Form is invalid
+  } else if (statusInput.value === "") {
+    msg.innerHTML = "You must choose a status!";
+    isValid = false; // Form is invalid
+  } else {
+    msg.innerHTML = ""; // Clear previous error message
+  }
 
-            calendar.appendChild(daySquare);
-        }
+  // Only proceed if form is valid
+  if (isValid) {
+    if (currentTaskID) {
+      updateTask(currentTaskID);
+    }
+    return true; // Indicate that form processing should continue
+  }
+
+  // Prevent modal from closing by not calling hide function
+  return false; // Indicate form is not valid
+};
+
+
+let createNewTask = () => {
+  let userID = localStorage.getItem('currentUserID');
+  let textInput = document.getElementById("textInput");
+  let dateInput = document.getElementById("dateInput");
+  let textArea = document.getElementById("textArea");
+  let difficulty = document.getElementById("difficulty");
+  let statusInput = document.getElementById("statusInput")
+
+  const taskData = {
+    userID,
+    taskName: textInput.value,
+    taskDesc: textArea.value,
+    taskDateDue: dateInput.value,
+    TaskCreditsReward: difficulty.value,
+    taskStatus: statusInput.value,
+  };
+  fetch('/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+    return response.json();
+  }).then(data => {
+    alert('Task created successfully');
+    displayTasks();
+    resetForm();
+  }).catch(error => {
+    console.error('Error:', error);
+    alert('An error occurred while creating the task');
+  });
+};
+
+
+let updateTask = (taskID) => {
+  let textInput = document.getElementById("textInput");
+  let dateInput = document.getElementById("dateInput");
+  let textArea = document.getElementById("textArea");
+
+  const taskData = {
+    taskName: textInput.value,
+    taskDesc: textArea.value,
+    taskDateDue: dateInput.value
+  };
+
+  fetch(`/tasks/` + taskID, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to update task');
+      }
+      return response.text();
     })
-    .catch(error => console.error('Error fetching tasks:', error));
+    .then(data => {
+      alert(data); // Display success message
+      displayTasks(); // Refresh task list
+      resetForm(); // Clear form fields
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('An error occurred while updating the task');
+    });
+};
+
+// Delete task
+let deleteTask = (taskID) => {
+  fetch(`/tasks/` + taskID, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      isTaskDeleted: true
+    })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete task');
+      }
+      return response.text();
+    })
+    .then(data => {
+      alert(data); // Display success message
+      displayTasks(); // Refresh task list
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('An error occurred while deleting the task');
+    });
+};
+
+let completeTask = async (taskID) => {
+  let rollData = await readData()
+
+  fetch(`/tasks/` + taskID)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch task data for completion');
+      }
+      return response.json();
+    })
+    .then(taskData => {
+      // Store the TaskCreditsReward temporarily
+      const taskCreditsReward = taskData.TaskCreditsReward;
+      console.log("TaskCreditsReward:", taskCreditsReward); // Log it or store it as needed
+
+      //updates user balance
+      console.log(userID + taskCreditsReward) 
+      fetch(('/userdatas/' + userID), {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            credits: rollData.credits + taskCreditsReward
+        })
+      })
+
+      // Now update the task as completed
+      return fetch(`/tasks/` + taskID, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          taskStatus: 'Completed' // Update the status to Completed
+        }),
+      });
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to update task status to completed');
+      }
+      alert('Task marked as completed successfully!');
+      displayTasks(); // Refresh the task list to show the updated status
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('An error occurred while completing the task');
+    });
+};
+
+let currentTaskID = null;
+function showModal() {
+  // Check if the elements exist before accessing their properties
+  const profileNameElement = document.getElementById("profileName");
+  const profileEmailElement = document.getElementById("profileEmail");
+  const profileDateCreatedElement = document.getElementById("profileDateCreated");
+  const profileTasksCompletedElement = document.getElementById("profileTasksCompleted");
+  const coinsElement = document.getElementById("coins");
+
+  // Check if elements are found before accessing properties
+  if (profileNameElement && profileEmailElement && profileDateCreatedElement && profileTasksCompletedElement && coinsElement) {
+    // Set innerText properties
+    profileNameElement.innerText = "Some Name";
+    profileEmailElement.innerText = "email@example.com";
+    profileDateCreatedElement.innerText = "Some Date";
+    profileTasksCompletedElement.innerText = "Some Tasks Completed";
+    coinsElement.innerText = "Some Coins";
+    // Other actions in showModal()
+  } else {
+    console.error("Elements not found");
+  }
 }
 
-function initButtons() {
-    document.getElementById('nextButton').addEventListener('click', () => {
-        nav++;
-        load();
-    });
-
-    document.getElementById('backButton').addEventListener('click', () => {
-        nav--;
-        load();
-    });
-
-    document.getElementById('returnButton').addEventListener('click', () => {
-        window.location.href = "/pages/main";
+function editTask(taskID) {
+  fetch(`/tasks/` + taskID)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch task data');
+      }
+      return response.json();
+    })
+    .then(taskData => {
+      const dateDue = new Date(taskData.taskDateDue).toISOString().split('T')[0];
+      // Populate form fields with task data for editing
+      document.getElementById("textInput").value = taskData.taskName;
+      document.getElementById("dateInput").value = dateDue;
+      document.getElementById("textArea").value = taskData.taskDesc;
+      // Set global task ID to current editing task
+      currentTaskID = taskID; // This is important
+      showModal();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('An error occurred while fetching task data');
     });
 }
 
-initButtons();
-load();
+
+
+
+let resetForm = () => {
+  document.getElementById("textInput").value = "";
+  document.getElementById("dateInput").value = "";
+  document.getElementById("textArea").value = "";
+  // Reset the currentTaskID
+  currentTaskID = null; // This ensures the form is reset for creation
+};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const editButtons = document.querySelectorAll(".edit-btn");
+
+  // Attach click event listener to each edit button
+  editButtons.forEach(button => {
+    button.addEventListener("click", function () {
+      // Extract task ID from the button's dataset
+      const taskID = button.dataset.taskId;
+      // Call editTask function with the task ID
+      editTask(taskID);
+    });
+  });
+
+  displayTasks();
+});
+
+function displayTasks() {
+  let userID = localStorage.getItem('currentUserID');
+  let tasksContainer = document.getElementById("tasks");
+  tasksContainer.innerHTML = ''; // Clear existing tasks
+
+  fetch(`/tasks/getUser/` + userID)
+      .then(response => response.json())
+      .then(tasks => {
+          tasks.forEach(task => {
+              // Skip tasks marked as deleted
+              if (task.isTaskDeleted || task.taskStatus === 'Completed') return;
+
+              const taskElement = document.createElement("div");
+              taskElement.id = `task-${task._id}`;
+
+              const taskNameHeader = document.createElement("h3");
+              taskNameHeader.textContent = task.taskName;
+              taskElement.appendChild(taskNameHeader);
+
+              const taskDescParagraph = document.createElement("p");
+              taskDescParagraph.textContent = task.taskDesc;
+              taskElement.appendChild(taskDescParagraph);
+
+              const taskDueSpan = document.createElement("span");
+              const dueDate = new Date(task.taskDateDue);
+              const currentDate = new Date();
+              currentDate.setHours(0, 0, 0, 0); // Normalize current date to start of day for comparison
+
+              // Check if the task is overdue
+              if (dueDate < currentDate) {
+                  taskDueSpan.textContent = `Due: ${dueDate.toLocaleDateString()} - Overdue`;
+                  taskDueSpan.style.color = 'red'; // Change color to red or any visual indication
+              } else {
+                  taskDueSpan.textContent = `Due: ${dueDate.toLocaleDateString()}`;
+              }
+
+              taskElement.appendChild(taskDueSpan);
+
+              const editButton = document.createElement("button");
+              editButton.textContent = "Edit";
+              editButton.setAttribute("type", "button");
+              editButton.classList.add("edit-btn", "btn", "btn-primary");
+              editButton.setAttribute("data-bs-toggle", "modal");
+              editButton.setAttribute("data-bs-target", "#form");
+              editButton.setAttribute("data-task-id", task._id);
+              editButton.addEventListener("click", () => editTask(task._id));
+              taskElement.appendChild(editButton);
+
+              const deleteButton = document.createElement("button");
+              deleteButton.textContent = "Delete";
+              deleteButton.classList.add("delete-btn", "btn", "btn-danger");
+              deleteButton.addEventListener("click", () => deleteTask(task._id));
+              taskElement.appendChild(deleteButton);
+
+              const completeButton = document.createElement("button")
+              completeButton.textContent = "Complete Task";
+              completeButton.setAttribute("type", "button");
+              completeButton.classList.add("complete-btn", "btn", "btn-primary");
+              completeButton.addEventListener("click", () => completeTask(task._id));
+              taskElement.appendChild(completeButton);
+
+              tasksContainer.appendChild(taskElement);
+          });
+      })
+      .catch(error => {
+          console.error('Error fetching tasks:', error);
+      });
+}
+
+window.onload = function() {
+  userID = localStorage.getItem("currentUserID")
+  if (userID == null || userID == undefined) {
+      window.location.href = "/"
+      console.log(userID)
+  } else {
+      console.log("no user")
+  }
+};
